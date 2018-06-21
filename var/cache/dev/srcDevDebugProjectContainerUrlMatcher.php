eff32d8704d31076c5b7a08a77021c61f296030b
assafb <assaf.berkovitz@kaltura.com>
2018-06-21 23:11:11 +0300

    add additional files

---
+++ var/cache/dev/srcDevDebugProjectContainerUrlMatcher.php
@@ -0,0 +1,50 @@
+<?php
+
+use Symfony\Component\Routing\Exception\MethodNotAllowedException;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\RequestContext;
+
+/**
+ * This class has been auto-generated
+ * by the Symfony Routing Component.
+ */
+class srcDevDebugProjectContainerUrlMatcher extends Symfony\Bundle\FrameworkBundle\Routing\RedirectableUrlMatcher
+{
+    public function __construct(RequestContext $context)
+    {
+        $this->context = $context;
+    }
+
+    public function match($rawPathinfo)
+    {
+        $allow = array();
+        $pathinfo = rawurldecode($rawPathinfo);
+        $trimmedPathinfo = rtrim($pathinfo, '/');
+        $context = $this->context;
+        $request = $this->request ?: $this->createRequest($pathinfo);
+        $requestMethod = $canonicalMethod = $context->getMethod();
+
+        if ('HEAD' === $requestMethod) {
+            $canonicalMethod = 'GET';
+        }
+
+        if (0 === strpos($pathinfo, '/stories')) {
+            // stories_post
+            if ('/stories' === $pathinfo) {
+                return array (  '_controller' => 'App\\Controller\\StoriesController::post',  '_route' => 'stories_post',);
+            }
+
+            // stories_get
+            if (preg_match('#^/stories/(?P<id>\\d+)$#sD', $pathinfo, $matches)) {
+                return $this->mergeDefaults(array_replace($matches, array('_route' => 'stories_get')), array (  '_controller' => 'App\\Controller\\StoriesController::get',));
+            }
+
+        }
+
+        if ('/' === $pathinfo && !$allow) {
+            throw new Symfony\Component\Routing\Exception\NoConfigurationException();
+        }
+
+        throw 0 < count($allow) ? new MethodNotAllowedException(array_unique($allow)) : new ResourceNotFoundException();
+    }
+}
